aa3bea95a614fef13f6b9df2f2e2a30635424bb3
Alex <trainofbird@gmail.com>
2023-04-22 19:18:23 +0300

    Задача 3 завершена

---
+++ Ex003/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 34, 64, 33, 23, 11, 65, 61};
+
+int size = array.Length;
+int find = 23;
+
+int index = 0;
+
+while (index < size)
+{
+    if (array[index] == find);
+    {
+        Console.WriteLine(index);
+    }
+    // index = index + 1;
+    index++;
+}
